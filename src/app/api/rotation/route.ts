514296d26f6087207c74afd9f3055e295c5598c5
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-12-18 14:53:25 +0900

    chore: apikey 이름 변경

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   // Riot Games API의 챔피언 로테이션 정보를 가져오는 엔드포인트입니다.
   const URL = "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations";
-  const API_KEY = process.env.RIOT_API_KEY!;
+  const API_KEY = process.env.NEXT_PUBLIC_RIOT_API_KEY!;
 
   const response = await fetch(URL, {
     headers: {
